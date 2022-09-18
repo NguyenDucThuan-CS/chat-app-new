d75ce4639fe251f7d6bc6b6736c89d6998bcd9d4
thuan.nguyen <thuan.nguyen@yoot.vn>
2022-09-18 16:15:19 +0700

    update

--- app/src/server.js
+++ app/src/server.js
@@ -8,6 +8,14 @@ const pathPublicDirectory  = path.join(__dirname, "../public");
 app.use(express.static(pathPublicDirectory));
 const httpServer = createServer(app);
 
+const io = new Server(httpServer, {
+    
+})
+
+
+io.on("connection", (socket) => {
+    console.log("new client connect");
+})
 
 httpServer.listen(5000, () => {
     console.log(`app run on port 5000`);
