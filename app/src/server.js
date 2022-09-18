b7849e30691770034e24d7af6e96d101d5465817
thuan.nguyen <thuan.nguyen@yoot.vn>
2022-09-18 15:51:05 +0700

    update

--- app/src/server.js
+++ app/src/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const path = require("path");
+const { createServer } = require('http');
 const app = express();
+const { Server } =  require("socket.io");
 
-app.listen(5000, () => {
-    console.log(`app run on port 5000`)
+const pathPublicDirectory  = path.join(__dirname, "../public");
+app.use(express.static(pathPublicDirectory));
+const httpServer = createServer(app);
+
+
+httpServer.listen(5000, () => {
+    console.log(`app run on port 5000`);
 })
